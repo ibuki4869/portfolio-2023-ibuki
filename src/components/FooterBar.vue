4729bf38823330a5c7bfdf8778113f61cfa3cf41
Ibuki <50740991+ibuki4869@users.noreply.github.com>
2023-02-03 16:48:08 +0900

    Add sns links for footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,13 +1,37 @@
 <template>
-  <footer>- Mori Ibuki -</footer>
+  <div class="foot-area">
+    <div class="sns">
+      <a
+        href="https://jp.linkedin.com/in/ibuki-mori?trk=profile-badge"
+        target="_blank"
+        ><img src="@/assets/logos/LI-In-Bug.png"
+      /></a>
+      <a href="https://github.com/ibuki4869" target="_blank"
+        ><img src="@/assets/logos/github-mark.png"
+      /></a>
+    </div>
+    <div class="foot">
+      <footer>- Mori Ibuki -</footer>
+    </div>
+  </div>
 </template>
   
   
   
 <style lang="scss">
+.sns {
+  height: 50px;
+}
+.sns img {
+  height: 80%;
+  margin: 5px;
+}
+.foot {
+  height: 30px;
+}
 .footer {
+  margin: auto;
   text-align: center;
-  margin-top: 50px;
 }
 </style>
   
